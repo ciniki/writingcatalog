8b1e83ccd6a6a73cb770501adde3d5773780ba64
Andrew Rivett <veggiefrog@gmail.com>
2015-11-03 08:12:21 -0500

    Updated UI forms for image field

--- ui/images.js
+++ ui/images.js
@@ -16,7 +16,7 @@ function ciniki_writingcatalog_images() {
 		this.edit.data = {};
 		this.edit.writingcatalog_id = 0;
 		this.edit.sections = {
-			'_image':{'label':'Image', 'fields':{
+			'_image':{'label':'Image', 'type':'imageform', 'fields':{
 				'image_id':{'label':'', 'type':'image_id', 'hidelabel':'yes', 'controls':'all', 'history':'no'},
 				}},
 			'info':{'label':'Information', 'type':'simpleform', 'fields':{
